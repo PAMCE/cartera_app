149b2695a119a8e662cea6e6f0c62aa62c8d863a
pablo <remagforce@gmail.com>
2020-10-27 16:07:05 -0600

    agregue  spinner

--- app/src/main/java/com/progra/mi_cartera_app/MainActivity.java
+++ app/src/main/java/com/progra/mi_cartera_app/MainActivity.java
@@ -3,12 +3,31 @@ package com.progra.mi_cartera_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+    //declaracion de los elementos que voy a utilizar
+    public Spinner spinner1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //comunicacion con los elementos a utulizar
+        spinner1 = (Spinner)findViewById(R.id.spinner);
+
+
+
+        //introduzco los valores al spinner
+        String [] Meses = {"Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item_spiners,Meses);
+        spinner1.setAdapter(adapter);
+
     }
 }
