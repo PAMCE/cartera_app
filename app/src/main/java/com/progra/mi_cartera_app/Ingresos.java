7e26a515df119da9d498496846bbc4f266fdb620
pablo <remagforce@gmail.com>
2020-10-28 09:06:37 -0600

    agregue spinner al 2do activity

--- app/src/main/java/com/progra/mi_cartera_app/Ingresos.java
+++ app/src/main/java/com/progra/mi_cartera_app/Ingresos.java
@@ -2,13 +2,41 @@ package com.progra.mi_cartera_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Ingresos extends AppCompatActivity {
+
+
+    public Spinner spinner2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingresos);
+
+
+
+
+    //comunicacion con el spinner
+        spinner2 = (Spinner)findViewById(R.id.spinner2);
+
+        String [] Ingresos = {"Ingreso pasivo", "Ingreso activo"};
+
+        ArrayAdapter <String> adapter = new ArrayAdapter<>(this, R.layout.spinner_item_spiners, Ingresos);
+        spinner2.setAdapter(adapter);
+
     }
+
+
+
+    //metodos para navegar entre activitys
+    public void Inicio (View view){
+        Intent inicio = new Intent(this, MainActivity.class);
+        startActivity(inicio);
+    }
+
 }
